6eb8aa0d35ba07154a538ae48615c17d3f774424
Blake Smith <besmith43@gmail.com>
2019-10-04 13:29:48 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generate_NACException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
