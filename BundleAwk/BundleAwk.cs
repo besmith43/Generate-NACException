8ff823c227916a8f3f7f81a5b2958e46f8e90d61
Blake Smith <besmith43@gmail.com>
2019-10-14 10:35:34 -0500

    playing with the bash escape characters for a single quote

--- BundleAwk/BundleAwk.cs
+++ BundleAwk/BundleAwk.cs
@@ -7395,8 +7395,8 @@ AAAAAAAAAAAAAAAAAAAAAA==
         {
             //ping "ip-address"; arp -a | grep "\<149.149.140.5\>" | awk '{print $4}'
 
-            Console.WriteLine("bash " + "-c \'ping -c 4 " + printerIP + "; arp -a | grep \"\\<" + printerIP + "\\>\" | awk \'\"\'\"\'{print $4}\'\"\'\"\'\'");
-            var awkProcess = System.Diagnostics.Process.Start("bash", "-c \'ping -c 4" + printerIP + "; arp -a | grep \"\\<" + printerIP + "\\>\" | awk \'\"\'\"\'{print $4}\'\"\'\"\'\'");
+            Console.WriteLine("bash " + "-c \'ping -c 4 " + printerIP + "; arp -a | grep \"\\<" + printerIP + "\\>\" | awk \'\"\'\"\'{print $4}\'\"\'\"\'");
+            var awkProcess = System.Diagnostics.Process.Start("bash", "-c \'ping -c 4" + printerIP + "; arp -a | grep \"\\<" + printerIP + "\\>\" | awk \'\"\'\"\'{print $4}\'\"\'\"\'");
             awkProcess.StartInfo.RedirectStandardOutput = true;
             awkProcess.Start();
             
